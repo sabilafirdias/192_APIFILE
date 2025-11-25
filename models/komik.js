59580e34aeed5324d604667141b989dba27c9ce0
sabilafirdias <sabilafirdias@gmail.com>
2025-11-25 14:40:57 +0700

    membuat komik.js

---
+++ models/komik.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+    const Komik = sequelize.define("Komik", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        judul: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        penulis: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        deskripsi: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        imageType: DataTypes.STRING,
+        imageName: DataTypes.STRING,
+        imageData: DataTypes.BLOB('long'),
+    }, {
+        tableName: 'komik'
+    });
+    return Komik;
+}
