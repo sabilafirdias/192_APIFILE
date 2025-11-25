2caa73483e3ea54bb4d86891d1d920219525e28c
sabilafirdias <sabilafirdias@gmail.com>
2025-11-25 14:41:44 +0700

    membuat index.js di folder utama

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const connectDatabase = require('./config/db');
+// const apiRoute = require('./routes/api');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// app.use('/api/v1', apiRoute);
+
+async function startServer() {
+    await connectDatabase();
+    app.listen(PORT, () => {
+        console.log(`Server running at http://localhost:${PORT}`);
+    });
+}
+
+startServer();
