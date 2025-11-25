15ec90e1d48d3708262074abc86112688fed308f
sabilafirdias <sabilafirdias@gmail.com>
2025-11-25 15:40:33 +0700

    membuat komikService

---
+++ services/komikService.js
@@ -0,0 +1,68 @@
+async function createKomik(database, komikData) {
+    const { judul, deskripsi, penulis, imageType, imageName, imageData } = komikData;
+
+    if (!judul || !deskripsi || !penulis) {
+        throw new Error('Judul, deskripsi, dan penulis wajib diisi');
+    }
+
+    const newKomik = await database.Komik.create({
+        judul,
+        deskripsi,
+        penulis,
+        imageType: imageType || null,
+        imageName: imageName || null,
+        imageData: imageData || null,
+    });
+
+    return newKomik;
+}
+
+async function getAllKomik(database) {
+    const komiks = await database.Komik.findAll();
+
+    return komiks.map(k => {
+        if (k.imageData) {
+            k.imageData = k.imageData.toString('base64');
+        }
+        return k;
+    });
+}
+
+async function getKomikById(database, id) {
+    const komik = await database.Komik.findByPk(id);
+    if (!komik) throw new Error('Komik tidak ditemukan');
+
+    if (komik.imageData) {
+        komik.imageData = komik.imageData.toString('base64');
+    }
+
+    return komik;
+}
+
+async function updateKomik(database, id, komikData) {
+    const komik = await database.Komik.findByPk(id);
+    if (!komik) {
+        throw new Error(`Komik dengan ID ${id} tidak ditemukan`);
+    }
+
+    await komik.update(komikData);
+    return komik;
+}
+
+async function deleteKomik(database, id) {
+    const komik = await database.Komik.findByPk(id);
+    if (!komik) {
+        throw new Error(`Komik dengan ID ${id} tidak ditemukan`);
+    }
+
+    await komik.destroy();
+    return { message: `Komik dengan ID ${id} telah dihapus` };
+}
+
+module.exports = {
+    createKomik,
+    getAllKomik,
+    getKomikById,
+    updateKomik,
+    deleteKomik,
+};
